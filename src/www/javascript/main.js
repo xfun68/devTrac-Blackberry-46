dc7c59123fcaafa53459d9601b950bfc9f909563
Sachin Dharmapurikar <sachin@thoughtworks.com>
2011-03-08 13:45:24 +0530

    Jquery / Phonegap is integrated. First screen is displayed.

--- src/www/javascript/main.js
+++ src/www/javascript/main.js
@@ -1,26 +1,12 @@
-// invoked when application is resumed (brought to foregroud)
-function doResume() {
-    console.log('doResume()');
-}
+$(document).ready(function() {
+    init();
+});
 
-// invoked when application is paused (sent to background)
-function doPause() {
-    console.log('doPause()');
-}
-
-// register PhoneGap event listeners when DOM content loaded
 function init() {
-	debug("Whats up?");
-    console.log('init()');
-    document.addEventListener("resume", doResume, false);
-    document.addEventListener("pause", doPause, false);
+	alert("Yo! I'm loaded.");
     hideScreensExceptLogin();
     // Initialize all application events
     initializeAll();
-}
-
-function unload() {
-    console.log('unload()');
 }
 
 function hideScreensExceptLogin() {
