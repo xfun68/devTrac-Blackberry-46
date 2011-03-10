d6e99d09f6ff32c1c3a470bbee53440d42b4a39f
Jayalaxmi Dinni <jdinni@thoughtworks.com>
2011-03-10 11:16:14 +0530

    Adding html mockups

---
+++ src/DevTRac/DevTrac.java
@@ -0,0 +1,10 @@
+package DevTRac;
+
+public class DevTrac{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args){
+    }
+}
