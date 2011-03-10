aded10cc714dfcc599117d6800dc809c42940ae2
Sachin Dharmapurikar <sachin@thoughtworks.com>
2011-03-10 17:14:18 +0530

    Added links to mock ui

--- src/www/javascript/lib/initializer.js
+++ src/www/javascript/lib/initializer.js
@@ -3,4 +3,7 @@ function initializeApplicationEvents(){
     $("#logout").click(devtrac.loginController.logout);
     $("#add_new_site_button").click(siteController.add);
     $("#sites_to_visit_button").click(siteController.list);
+	$("#location1").click(questionsController.show);
+	$("#location2").click(questionsController.show);
+	$("#questions-submit").click(siteController.list);
 }
