d0575a1d20ac66251fbdb04b8b218907383a896c
Jayalaxmi Dinni <jdinni@thoughtworks.com>
2011-03-18 11:43:21 +0530

    Site visit list feedback changes

--- src/www/javascript/controllers/fieldTripController.js
+++ src/www/javascript/controllers/fieldTripController.js
@@ -12,7 +12,7 @@ fieldTripController.showTripReports = function(){
     }
 };
 
-fieldTripController.display = function(response){
+/*fieldTripController.display = function(response){
     if (response) {
         devtrac.fieldTrip = JSON.parse(response);
         $("#trip_title").html(devtrac.fieldTrip.title);
@@ -26,6 +26,30 @@ fieldTripController.display = function(response){
                 html += "<span class=\"done\"><img src=\"css/images/icon_tick.gif\"/></span>";
             }
             html += "</li>";
+            $("#site_list").append(html);
+        }
+        screens.show("sites_to_visit");
+        attachClickEvents("site_item", showSiteDetailScreen);
+    }
+    else {
+        alert("You don't have active field trips.");
+        devtrac.loginController.logout();
+    }
+}*/
+
+fieldTripController.display = function(response){
+    if (response) {
+        devtrac.fieldTrip = JSON.parse(response);
+        $("#trip_title").html(devtrac.fieldTrip.title);
+        $("#site_list").html("");
+        for (var id in devtrac.fieldTrip.sites) {
+            var site = devtrac.fieldTrip.sites[id];
+            var siteId = site.id ? site.id : site.name;
+            var html = "<div class=\"grid_row\" id=\"" + siteId + "\" ><div class=\"site_item link col1\">" + site.name + "</div><div class=\"col2\">" + site.type + "</div>";
+            if (site.complete) {
+                html += "<div class=\"done col3\"><img src=\"css/images/icon_tick.gif\"/></div>";
+            }
+            html += "</div>";
             $("#site_list").append(html);
         }
         screens.show("sites_to_visit");
